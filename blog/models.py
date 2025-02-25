b49b401676fb5788d13626f0caa468b47a2548d3
hassandn <hassandn6350@gmail.com>
2025-02-25 09:25:12 +0400

    create project and config it and implement some parts of blog model

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Post(models.Model):
+    STATUS_CHOICES =(
+        ('pub', 'Published'),#first one is for database second one is for display 
+        ('drf', 'Draft'),
+    )
+    
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    # author
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_CHOICES)
+    # status

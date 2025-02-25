c9c0e832c3dcf8488a8d71fccfb07e7d370b1e19
hassandn <hassandn6350@gmail.com>
2025-02-25 12:34:54 +0400

    add db and pycache to gitignore 2.add post model to admin pannel and customize it

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,6 @@ class Post(models.Model):
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
     status = models.CharField(choices=STATUS_CHOICES,max_length=3)
+
+    def __str__(self):
+        return self.title

5bb1c31150dd8b96798afdbdb8f4a862638c8b94
hassandn <hassandn6350@gmail.com>
2025-03-04 15:57:40 +0400

    implement customize admin pannel for showing model

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Post
 
+
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'datetime_created', 'author')
     list_filter = ('status', 'datetime_created', 'author')
-
-
-admin.site.register(Post, PostAdmin)
+    ordering = ('-datetime_created', )

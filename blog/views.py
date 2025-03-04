7ccf800e04fa38d41334d3b8c487e04774dc2833
hassandn <hassandn6350@gmail.com>
2025-03-04 15:58:20 +0400

    implement simple view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list_view(request):
+    posts = Post.objects.all()
+    context = {
+        'posts_list': posts
+    }
+    return render(request, 'blog/posts_list.html', context)

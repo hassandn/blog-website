9e0021b9a761f433209f53c14174a6fec518d8a3
hassandn <hassandn6350@gmail.com>
2025-03-13 14:29:20 +0400

    add detail view option

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list_view, name='post_list'),
+    path('<int:pk>/', views.post_detail_view, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,12 @@ def post_list_view(request):
         'posts_list': posts
     }
     return render(request, 'blog/posts_list.html', context)
+
+def post_detail_view(request, pk):
+
+    post = Post.objects.get(pk=pk)
+    context = {
+        "post" : post,
+    }
+    return  render(request=request, template_name= "blog/post_detail.html", context=context, status=200)
+

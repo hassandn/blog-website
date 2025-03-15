231ecec7bc6c8826e17bcd14e9961c157ef6f78b
hassandn <hassandn6350@gmail.com>
2025-03-15 11:49:18 +0400

    implement three ways to show 404

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 
 def post_list_view(request):
     posts = Post.objects.all()
@@ -9,10 +11,34 @@ def post_list_view(request):
     return render(request, 'blog/posts_list.html', context)
 
 def post_detail_view(request, pk):
+    
+    # try:
+    #     post = Post.objects.get(pk=pk)
+    # except:
+    #     post = None
+    #     print("exception")
+    # return  render(request=request, template_name= "blog/post_detail.html", context=context, status=200)
 
-    post = Post.objects.get(pk=pk)
+    # try:
+    #     post = Post.objects.get(pk=pk)
+    # except Post.DoesNotExist:
+    #     post = None
+    #     print("exception")
+    # return  render(request=request, template_name= "blog/post_detail.html", context=context, status=200)
+
+    # try:
+    #     post = Post.objects.get(pk=pk)
+    # except ObjectDoesNotExist:
+    #     post = None
+    #     print("exception")
+    # return  render(request=request, template_name= "blog/post_detail.html", context=context, status=200)
+
+
+    post = get_object_or_404(Post, pk=pk)
     context = {
         "post" : post,
     }
+    
+    
     return  render(request=request, template_name= "blog/post_detail.html", context=context, status=200)
 

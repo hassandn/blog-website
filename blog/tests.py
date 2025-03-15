c0f78e28bf96524b482f04fb68ace164ec057f3f
hassandn <hassandn6350@gmail.com>
2025-03-15 12:58:34 +0400

    complete blog test

--- blog/tests.py
+++ blog/tests.py
@@ -24,3 +24,23 @@ class BlogPostTest(TestCase):
     def test_list_url_by_name(self):
         rseponse = self.client.get(reverse('post_list'))
         
+    def test_post_title_on_blog_list_page(self):
+        response = self.client.get(reverse('post_list'))
+        self.assertContains(response, self.post1.title)
+        
+    def test_post_detail_url(self):
+        response = self.client.get(f'/blog/{self.post1.pk}/')
+        self.assertEqual(response.status_code, 200) 
+        
+    def test_post_detail_url_by_name(self):
+        response = self.client.get(reverse('post_detail', args=[self.post1.pk]))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_post_detail_on_blog_detail_page(self):
+        response = self.client.get(reverse('post_detail', args=[self.post1.pk]))
+        self.assertContains(response, self.post1.title)
+        self.assertContains(response, self.post1.text)
+        
+    def test_status_404_if_post_id_not_exist(self):
+        response = self.client.get(reverse('post_detail', args=[1000]))
+        self.assertEqual(response.status_code, 404)
